0c5071e1ab22ffe0feb6c13edb0efd5408647530
vik <hritvik111@gmail.com>
2026-01-09 20:39:43 +0530

    imp: handleDisconnect

--- server/src/handlers/index.ts
+++ server/src/handlers/index.ts
@@ -5,46 +5,64 @@ import { handlePrivateMessage } from "./privateMessage";
 import { handleCreateRoom } from "./createRoom";
 import { handleJoinRoom } from "./joinRoom";
 import { handleGroupMessage } from "./groupMessage";
+import { getSender, rooms, sockets, users } from "../state";
 
-export function handleMessage(ws: WebSocket, data: WebSocket.RawData){
+export function handleMessage(ws: WebSocket, data: WebSocket.RawData) {
+  let msg: ClientMessage;
 
-    let msg : ClientMessage;
+  try {
+    msg = JSON.parse(data.toString());
+  } catch {
+    ws.send(JSON.stringify({ type: "ERROR", message: "Invalid JSON" }));
+    return;
+  }
 
-    try{
-        msg = JSON.parse(data.toString());
-    }catch{
-        ws.send(JSON.stringify({ type: "ERROR", message: "Invalid JSON" }));
-        return;
+  switch (msg.type) {
+    case "JOIN": {
+      handleJoin(ws, msg.username);
+      break;
     }
-
-    switch(msg.type){
-        case "JOIN": {
-            handleJoin(ws, msg.username);
-            break;
-        }
-        case "PRIVATE_MESSAGE": {
-            handlePrivateMessage(ws, msg.toUserId, msg.content)
-            break;
-        }
-        case "GROUP_MESSAGE": {
-            handleGroupMessage(ws, msg.roomId, msg.content);
-            break;
-        }
-        case "CREATE_ROOM": {
-            handleCreateRoom(ws, msg.roomName);
-            break;
-        }
-        case "JOIN_ROOM": {
-            handleJoinRoom(ws, msg.roomId);
-            break;
-        }
-        default: {
-            // const _exhaustive: never = msg;
-            ws.send(JSON.stringify({ type: "ERROR", message: "Unknown message type" }));
-        }
+    case "PRIVATE_MESSAGE": {
+      handlePrivateMessage(ws, msg.toUserId, msg.content);
+      break;
     }
+    case "GROUP_MESSAGE": {
+      handleGroupMessage(ws, msg.roomId, msg.content);
+      break;
+    }
+    case "CREATE_ROOM": {
+      handleCreateRoom(ws, msg.roomName);
+      break;
+    }
+    case "JOIN_ROOM": {
+      handleJoinRoom(ws, msg.roomId);
+      break;
+    }
+    default: {
+      // const _exhaustive: never = msg;
+      ws.send(
+        JSON.stringify({ type: "ERROR", message: "Unknown message type" })
+      );
+    }
+  }
 }
 
-export function handleDisconnect(ws: WebSocket){
+export function handleDisconnect(ws: WebSocket) {
+  const sender = getSender(ws);
+  if (!sender) return;
 
+  for (const room of rooms.values()) {
+    room.members = room.members.filter((memberId) => memberId !== sender.id);
+
+    // Optional: delete empty rooms
+    if (room.members.length === 0) {
+      rooms.delete(room.id);
+    }
+  }
+
+  users.delete(sender.id);
+  const socketId = (ws as any).socketId;
+  if (socketId) {
+    sockets.delete(socketId);
+  }
 }
